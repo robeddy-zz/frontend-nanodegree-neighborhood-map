ec21d0235a29e13b8a1ad5f41cfdfc93d7ebbb30
RE <robeddy@gmail.com>
2016-11-27 22:26:41 -0500

    For Monday

--- js/app.js
+++ js/app.js
@@ -4,86 +4,146 @@ var hudsonCounty = [
 		name: 'Bayonne',
 		municipalType: 'City',
 		imgSrc: 'img/catB.jpg',
-		speakerNames: ['City Hall','Near 14A Turnpike Ext']
+		coords: {
+			lat: 40.685484,
+			lng: -74.100056
+		}
 	},
 	{
 		clickCount: 0,
-		name: 'East Newark',
-		municipalType: 'Borough',
-		imgSrc: 'img/catA.jpg',
-		speakerNames: ['None discovered yet']
+		name: 'Bayonne-City Hall',
+		municipalType: 'City',
+		imgSrc: 'img/catB.jpg',
+		coords: {
+			lat: 40.668556,
+			lng: -74.117720
+		}
 	},
 	{
 		clickCount: 0,
-		name: 'Guttenberg',
-		municipalType: 'Town',
+		name: 'Berry Lane Park',
+		municipalType: 'City',
 		imgSrc: 'img/catC.jpg',
-		speakerNames: ['None discovered yet']
+		coords: {
+			lat: 40.714493,
+			lng: -74.067334
+		}
+	},
+	{
+		clickCount: 0,
+		name: 'Exchange Place',
+		municipalType: 'City',
+		imgSrc: 'img/catB.jpg',
+		coords: {
+			lat: 40.717200,
+			lng: -74.034483
+		}
+	},
+	{
+		clickCount: 0,
+		name: 'Grove Street',
+		municipalType: 'City',
+		imgSrc: 'img/catB.jpg',
+		coords: {
+			lat: 40.718859,
+			lng: -74.041371
+		}
 	},
 	{
 		clickCount: 0,
 		name: 'Harrison',
 		municipalType: 'Town',
 		imgSrc: 'img/catC.jpg',
-		speakerNames: ['Near Blue Drawbridge']
+		coords: {
+			lat: 40.743598,
+			lng: -74.155948
+		}
 	},
 	{
 		clickCount: 0,
 		name: 'Hoboken',
 		municipalType: 'City',
 		imgSrc: 'img/catC.jpg',
-		speakerNames: ['Observer Highway']
+		coords: {
+			lat: 40.735497,
+			lng: -74.031049
+		}
 	},
 	{
 		clickCount: 0,
-		name: 'Jersey City',
+		name: 'Jersey City Heights',
 		municipalType: 'City',
-		imgSrc: 'img/catC.jpg',
-		speakerNames: ['Exchange Place','Five Corners','Grove Street','Jersey City Heights',
-		            'Liberty State Park','Lincoln Park','Newport']
+		imgSrc: 'img/catB.jpg',
+		coords: {
+			lat: 40.749134,
+			lng: -74.039336
+		}
 	},
 	{
 		clickCount: 0,
-		name: 'Kearny',
+		name: 'Kearny Health Department',
 		municipalType: 'Town',
 		imgSrc: 'img/catC.jpg',
-		speakerNames: ['Health Department','Jail','Library']
+		coords: {
+			lat: 40.773315,
+			lng: -74.143786
+		}
 	},
 	{
 		clickCount: 0,
-		name: 'North Bergen',
-		municipalType: 'Township',
-		imgSrc: 'img/catC.jpg',
-		speakerNames: ['None discovered yet']
-	},
-	{
-		clickCount: 0,
-		name: 'Secaucus',
-		municipalType: 'Town',
-		imgSrc: 'img/catC.jpg',
-		speakerNames: ['Outlets']
-	},
-	{
-		clickCount: 0,
-		name: 'Union City',
+		name: 'Liberty State Park',
 		municipalType: 'City',
-		imgSrc: 'img/catC.jpg',
-		speakerNames: ['None discovered yet']
+		imgSrc: 'img/catB.jpg',
+		coords: {
+			lat: 40.709475,
+			lng: -74.042902
+		}
 	},
 	{
 		clickCount: 0,
-		name: 'Weehawken',
-		municipalType: 'Township',
-		imgSrc: 'img/catC.jpg',
-		speakerNames: ['Ferry']
+		name: 'Lincoln Park',
+		municipalType: 'City',
+		imgSrc: 'img/catB.jpg',
+		coords: {
+			lat: 40.729630,
+			lng: -74.085907
+		}
 	},
 	{
 		clickCount: 0,
-		name: 'West New York',
-		municipalType: 'Town',
-		imgSrc: 'img/catC.jpg',
-		speakerNames: ['None discovered yet']
+		name: 'Newport',
+		municipalType: 'City',
+		imgSrc: 'img/catB.jpg',
+		coords: {
+			lat: 40.726500,
+			lng: -74.035106
+		}
 	}
+	// ,
+	// {
+	// 	clickCount: 0,
+	// 	name: 'South Kearny',
+	// 	municipalType: 'Town',
+	// 	imgSrc: 'img/catC.jpg'
+	// },
+	// {
+	// 	clickCount: 0,
+	// 	name: 'Kearny Library',
+	// 	municipalType: 'Town',
+	// 	imgSrc: 'img/catC.jpg'
+	// },
+	// {
+	// 	clickCount: 0,
+	// 	name: 'Secaucus Outlets',
+	// 	municipalType: 'Town',
+	// 	imgSrc: 'img/catC.jpg'
+	// },
+	// {
+	// 	clickCount: 0,
+	// 	name: 'Weehawken',
+	// 	municipalType: 'Township',
+	// 	imgSrc: 'img/catC.jpg'
+	// }
 ]
 
 
